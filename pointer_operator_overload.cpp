1da4913697d80ef1d1217688979a087bcc1d8285
abreathingcorpse <raul.rst11@gmail.com>
2022-11-14 16:50:50 -0600

    Played with the pointer_operator_overload script. Missing a couple of answers. Why does the index increases? What the heck is (Obj*)0?

--- pointer_operator_overload.cpp
+++ pointer_operator_overload.cpp
@@ -7,8 +7,9 @@ class Obj {
 	static int i, j, x; // The static variables are being shared throughout all of the instances of this class
 
 public:
-	void f() const { cout << i++ << endl; } // Constant functions should not be able to modify members, so why does this one work?
-	void g() const { cout << j++ << endl; } // Constant functions should not be able to modify members, so why does this one work?
+	// Apparently you are able to modify a static member if it's defined
+	void f() const { cout << i++ << endl; }  
+	void g() const { cout << j++ << endl; }  
 
 	// Test the const function, it should not be able to modify the members
 	// Apparently you are able to modify a static member if it's defined, though
@@ -43,38 +44,62 @@ class ObjContainer {
 class SmartPointer {
 	ObjContainer oc;
 	int index;
+	int test_int; // Figuring out if all integers will advance by one whenever the ++ operator is called
 
 	public:
 		SmartPointer(ObjContainer& objc) { // ObjContainer& objc should be the reference address to objc
 		// cout << "Constructor called." << endl;
 		oc = objc;
 		index = 0;	
+		test_int = 0;	
 	}
+		// Trying to figure out why is index increased
+		int get_index() {
+			return index;
+		}
+		int get_test_int() {
+			return test_int;
+		}
 
 	// Return value indicates end of list:
 	bool operator++() { // Prefix version
-		if ( index >= oc.a.size() ) return false; // oc has access to a, because of the friend class SmartPointer statement within the ObjContainer class
+		// cout << "++ Prefix version overload called." << endl;
+		cout << "index: " << index <<  " vs oc.a.size()" << oc.a.size() << endl; 
+		// cout << "++index: " << ++index <<  " vs oc.a[index]" << oc.a[index] << endl; 
+		// sp has access to oc, because it's given on its constructor
+		if ( index >= oc.a.size() ) {
+		cout << "Inside the if statement. index: " << index << endl; 
+		 return false;
+		}
 		if ( oc.a[++index] == 0 ) return false; // Why does this work? Does vector return 0 if trying to access a value outside of its size?
 		return true;
 	}
 
+	// When calling the Postfix version, you are actually calling the Prefix version
 	bool operator++(int) { // Postfix version
-		return operator++(); // Not sure what does this return. Did nothing got altered here?
+		// cout << "++ Postfix version overload called." << endl;
+		return operator++();  
 	}
+
 
 	// overload operator ->
 	Obj* operator->() const {
 		// It looks like index is being increased for some reason
-		// cout << "Index: " << index << endl;
+		// cout << "index inside the -> operator overload: " << index << endl;
+
 		// What is oc.a[index] ?
-		//cout << "oc.a[index]" << oc.a[index] << endl; // It is a reference address value from the vector a for a given index
-		// Don't understand anything here
+		// It is a reference address value from the vector a for a given index
+		cout << "oc.a[index]: " << oc.a[index] << endl; 
+		// Trying to understand what happens when the index is
+		// It looks like the reference address is 0 out of bounds
+		cout << "oc.a[10+index]: " << oc.a[10+index] << endl; 
+
 		if(!oc.a[index]) {
 			cout << "Zero value";	
-			return (Obj*)0;
+			return (Obj*)0; // What is (Obj*) 0?
 		}	
 		
-		return oc.a[index]; // This one is straightforward, though. Simply passes the value of a[index]
+		return oc.a[index]; // Passes the reference value of a[index]
 	}
 };
 
@@ -84,21 +109,23 @@ int main() {
 	ObjContainer oc;
 
 	// Test the const function
-	Obj obj_test;
-	cout << obj_test.get_x() << endl; // get_x() was actually able to modify the value of x
+	/*Obj obj_test;
+	cout << obj_test.get_x() << endl; // get_x() was actually able to modify the value of x, once x was defined */
 
 	for (int i=0; i < sz; i++) {
 		//cout << "&o[" << i << "]: " << &o[i] << endl;	
 		oc.add(&o[i]); // &o[i] is the reference address
 	}
-		
+
 	// Calls the constructor of the SmartPointer class
 	SmartPointer sp(oc); // Create an iterator
 	do {
+		// I'm trying to figure out why the index increased
+		cout << "sp.index: " << sp.get_index() << endl;
+		cout << "sp.test_int: " << sp.get_test_int() << endl;
 		// Use the overloaded -> operator
 		sp->f(); // smart pointer call
 		sp->g();
-		//sp->get_x(); // In order for get_x() to be callable, we need to have x defined
 	} while (sp++); // Use the ++ operator overload but, which one is it? ++x or x++? It should be x++ only
 
 	return 0;
